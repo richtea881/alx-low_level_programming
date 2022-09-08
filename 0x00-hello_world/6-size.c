21187e6a6777115b419f33abfb1bbe8c33d6ac36
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-09-08 15:20:53 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 *
 * Description: using the main function
 * this program prints “Programming is like building a multilingual puzzle”
-* Return: 0
+* return: 0
 */
 int main(void)
 {
@@ -17,8 +17,8 @@ int main(void)
 
 	printf(“Size of a char: %ld byte(s)\n”, sizeof(c));
 	printf(“Size of an int: %ld byte(s)\n”, sizeof(i));
-	printf(“Size of a long: %ld byte(s)\n”, sizeof(li));
-	printf(“Size of a long long: %ld byte(s)\n”, sizeof(lli));
+	printf(“Size of a long int: %ld byte(s)\n”, sizeof(li));
+	printf(“Size of a long long int: %ld byte(s)\n”, sizeof(lli));
 	printf(“Size of a float: %ld byte(s)\n”, sizeof(f));
 	return (0);
 }
