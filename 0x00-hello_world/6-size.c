0290155d2ed5cd207d092681a1446f8689f4b9cf
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-09-09 09:14:59 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 /**
-* Description: main - Prints the stdout
-* Return: 0 if successful
+* main - entry point
+* Return: 0
 */
 int main(void)
 {
+	char a;
+	int b;
+	long c;
+	long long d;
+	float f;
+
 	printf(“Size of a char: %d byte(s)\n”, sizeof(char));
 	printf(“Size of an int: %d byte(s)\n”, sizeof(int));
-	printf(“Size of a long int: %d byte(s)\n”, sizeof(long int));
-	printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long int));
+	printf(“Size of a long int: %d byte(s)\n”, sizeof(long));
+	printf(“Size of a long long int: %d byte(s)\n”, sizeof(long long));
 	printf(“Size of a float: %d byte(s)\n”, sizeof(float));
-	return (0);
+	Return (0);
 }
