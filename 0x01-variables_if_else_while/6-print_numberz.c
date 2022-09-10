418bf26139b4e157de4b8d2b91910d7ce72a7f4b
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-09-09 22:43:30 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succcess)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0' ; d <= ; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
