9ab80e2187d2b4fbe4ecbda1d7b2cc8e8c51ad71
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-09-14 16:22:22 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print _sign - function to check for a sign of a number
+ * @c: is the int that will use for the arguement of the function
+ * Return: 0
+ */
+int print_sign(int a)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (i);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
