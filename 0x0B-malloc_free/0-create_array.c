b7ea366730f6cc74ebbbda2dcbeb94bdd46b287f
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-10-04 16:17:58 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,24 +12,25 @@
 char *create_array(unsigned int size, char c)
 {
 		char *ptr;
-			unsigned int i;
+		unsigned int i;
 
-				if (size == 0)
-						{
-									return (NULL);
-										}
+			if (size == 0)
 
-					ptr = malloc(sizeof(c) * size);
+			{
+			return (NULL);
+			}
 
-						if (ptr == NULL)
-								{
-											return (NULL);
-												}
+			ptr = malloc(sizeof(c) * size);
 
-							for (i = 0; i < size; i++)
-									{
-												ptr[i] = c;
-													}
+			if (ptr == NULL)
+			{
+			return (NULL);
+			}
 
-								return (ptr);
+			for (i = 0; i < size; i++)
+			{
+			ptr[i] = c;
+			}
+
+			return (ptr);
 }
