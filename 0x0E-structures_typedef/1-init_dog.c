af41fe0f775af23bb5326f67b3eb7a12c9deb7f7
Odisi Anthony Enebimowei <doctorskull.z@gmail.com>
2022-10-10 20:55:12 -0700

    Task init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 /**
  * init_dog - initializes a variable of type struct dog.
  * @d: struct dog.
- * @name: name of the dog.
+ * @name: name of the dog..
  * @age: age of the dog.
  * @owner: owner of the dog.
  *
